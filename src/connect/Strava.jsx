a7a54ac53e8a3a5dfd98e3d76e5bdfc9fbeb0a76
Marek <kingeri@gmail.com>
2021-10-28 07:40:07 -0700

    Polar connect - wip

--- src/connect/Strava.jsx
+++ src/connect/Strava.jsx
@@ -16,7 +16,7 @@ const StravaConnect = () => {
         });
       });
     }
-  });
+  }, []);
 
   return (
     <section>
--- src/connect/index.jsx
+++ src/connect/index.jsx
@@ -3,22 +3,49 @@ import Button from '@mui/material/Button';
 
 import Api from '../api';
 
-const Connect = () => {
+const CONFIG = {
+  STRAVA: {
+    base: 'https://www.strava.com/oauth/authorize',
+    client_id: process.env.REACT_APP_STRAVA_CLIENT_ID,
+    redirect_uri: `${window.location.origin}/connect/strava`,
+    scope: 'activity:read'
+  },
 
+  POLAR: {
+    base: 'https://flow.polar.com/oauth2/authorization',
+    client_id: process.env.REACT_APP_POLAR_CLIENT_ID,
+    redirect_uri: `${window.location.origin}/connect/polar`,
+    scope: 'accesslink.read_all'
+  }
+};
+
+function buildURL(config) {
+  const params = [
+    `client_id=${config.client_id}`,
+    `response_type=code`,
+    `scope=${config.scope}`,
+    `redirect_uri=${encodeURIComponent(config.redirect_uri)}`
+  ];
+  return `${config.base}?${params.join('&')}`;
+};
+
+const Connect = () => {
   useEffect(() => {
     Api.ping();
   });
 
-  // Strava config
-  const client_id = process.env.REACT_APP_STRAVA_CLIENT_ID;
-  const scope = 'activity:read';
-  const redirect_uri=`${window.location.origin}/connect/strava`;
-  const strava = `https://www.strava.com/oauth/authorize?client_id=${client_id}&response_type=code&scope=${scope}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  const urls = {
+    polar: buildURL(CONFIG.POLAR),
+    strava: buildURL(CONFIG.STRAVA)
+  }
 
   return (
     <div>
-      <Button component={'a'} href={strava} variant="contained" size='large'>
+      <Button component={'a'} href={urls.strava} variant="contained" size='large'>
         Connect Strava
+      </Button>
+      <Button style={{opacity: 0}} component={'a'} href={urls.polar} variant="contained" size='large'>
+        Connect Polar
       </Button>
     </div>
   );
