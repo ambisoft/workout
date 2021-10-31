bce4162fdae41d200c5d0e7d735929c358ca3c8b
Marek <kingeri@gmail.com>
2021-10-31 14:45:31 -0700

    Join page: minor tweaks

--- src/Join.jsx
+++ src/Join.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
@@ -7,21 +8,27 @@ import { ThemeProvider } from '@mui/material/styles';
 
 import welcomeTheme from './ui/themes/welcome';
 
+const styles = {
+  mt: '2em',
+  mb: '1em',
+  width: 170,
+};
+
 const Join = () => (
   <ThemeProvider theme={welcomeTheme}>
     <div className="welcome">
-      <Typography variant='h1'>
+      <Typography variant='h1' textAlign='center'>
         Hooray! You're about to join our beta
       </Typography>
       <Typography variant='h3'>
         Hush hush - right now we are invite only
       </Typography>
-      <TextField id="access-code" label="Enter access code" />
-      <p>
-        <Button variant="contained" color='success' size='large'>
-          Submit
-        </Button>
-      </p>
+      <Box sx={styles}>
+        <TextField id="access-code" label="Enter access code" />
+      </Box>
+      <Button variant="contained" color='success' size='large'>
+        Submit
+      </Button>
     </div>
   </ThemeProvider>
 );
